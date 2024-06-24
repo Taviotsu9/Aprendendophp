5978acabfbd5927662826e66e10f49d4bb725a84
Taviotsu9 <otaviomalandrindossantos@gmail.com>
2024-06-24 18:10:24 -0300

    adicionei mais coisa

---
+++ Crud.php/phpgeral.php
@@ -0,0 +1,70 @@
+<?php 
+
+
+echo("ola tavinho"); 
+
+
+
+
+
+      //ambos escrevem algo
+
+
+
+      print ("Ola tavio"); 
+
+?>
+
+
+
+<?php
+
+print ( "este é um teste php <br />" ); //teste de de quebra linha br
+
+echo("este é o teste 2" );
+
+
+?>
+
+
+
+
+<?php
+
+$texto01 = "primeiro texto do script"; //$ cria variaveis
+
+print ($texto01); 
+
+
+?>
+
+
+
+
+<?php
+$texto01 = "primeiro texto do script"; // aqui a primeira variavel
+
+
+print ($texto01); 
+
+//print ( “texto02” );
+
+
+/*    comentário mais longo
+
+
+
+
+comentário mais longo 
+
+
+
+comentário mais longo */ 
+
+
+print('<span style="color:#00FFFF">texto01</span>'); //adiciona uma cor a variavel texto1
+?>
+
+
+
+
